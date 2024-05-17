8f829172f1234ab710ab5e7362b8a220b845f193
Cedde97 <ceddo.alt@gmail.com>
2024-05-17 20:42:46 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'worker_assistance_battery',
+  webDir: 'www'
+};
+
+export default config;
