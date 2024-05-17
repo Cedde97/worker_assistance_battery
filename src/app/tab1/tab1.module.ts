521a7826aa93068a15c16be0798613d3da18f5fb
Cedde97 <ceddo.alt@gmail.com>
2024-05-17 22:31:06 +0200

    init

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,6 +6,10 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { NgxScannerQrcodeModule, LOAD_WASM } from 'ngx-scanner-qrcode';
+
+// Necessary to solve the problem of losing internet connection
+LOAD_WASM().subscribe();
 
 @NgModule({
   imports: [
@@ -13,7 +17,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    NgxScannerQrcodeModule
   ],
   declarations: [Tab1Page]
 })
